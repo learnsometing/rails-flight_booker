301a324bbd1a949b1ef136a8c1ab20f8fab9e380
learnsometing <brianmonaccio@gmail.com>
2019-06-18 11:50:38 -0400

    Reconfigure query for PG and update production env to serve background image

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -4,14 +4,9 @@ class FlightsController < ApplicationController
   def index
     @flights = []
     if params[:search]
-      @flights = Flight.where('start_airport_id = :from AND
-                              finish_airport_id = :to AND
-                              date           like :depart_on AND
-                              seats_available  >= :tickets',
-                              from: params[:search][:from_airport_id],
-                              to: params[:search][:to_airport_id],
-                              depart_on: DateTime.parse(params[:search][:date]),
-                              tickets: params[:search][:num_tickets])
+      @flights = Flight.where(start_airport_id: params[:search][:from_airport_id],
+                              finish_airport_id: params[:search][:to_airport_id],
+                              date: DateTime.parse(params[:search][:date]))
       flash.now[:danger] = 'The flight you requested could not be found' if @flights.empty?
     end
   end
